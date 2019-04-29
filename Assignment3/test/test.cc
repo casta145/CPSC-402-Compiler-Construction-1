6ac7063c12265a5aaeddb06b5c15680d89cafe74
ColeGotelli <gotel100@mail.chapman.edu>
2019-04-29 10:12:22 -0700

    Adds test

---
+++ Assignment3/test/test.cc
@@ -0,0 +1,23 @@
+int main () {
+  int x = 0;
+  x++;
+  printInt(x);
+  return 0;
+}
+
+/*int main() {
+  int x = x;
+  return 0;
+}
+
+int f (int x, int x) {
+   return x;
+ }
+
+int main () {
+  return test();
+}
+
+int test() {
+  return 32;
+}*/
